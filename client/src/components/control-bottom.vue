dc7c24c5469c6d831900a737a82dd5920c56cdfb
jstella96 <jstella0530@naver.com>
2022-06-16 19:27:21 +0900

    design: control 버튼위치 변경

--- client/src/components/control-bottom.vue
+++ client/src/components/control-bottom.vue
@@ -1,9 +1,10 @@
 <template>
 <div class="record__control">
-    <div class="record__button record__play" :class="showInActive('play')" @click="changeState($event,'play')"></div>
-    <div class="record__button record__pause" :class="showInActive('pause')" @click="changeState($event,'pause')"></div>
-    <div class="record__button record__stop"  :class="showInActive('stop')" @click="changeState($event,'stop')"></div>
-</div>
+    <div v-if="state !== 'play'" class="record__button record__play" :class="showInActive('play')" @click="changeState($event,'play')"></div>
+ 
+    <div v-if="state !== 'ready' && state !== 'pause' " class="record__button record__pause" :class="showInActive('pause')" @click="changeState($event,'pause')"></div>
+    <div v-if="state !== 'ready'" class="record__button record__stop"  :class="showInActive('stop')" @click="changeState($event,'stop')"></div>
+</div> 
 </template>
 <script>
 export default {
