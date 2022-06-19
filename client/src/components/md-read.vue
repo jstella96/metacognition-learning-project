06d826d2204bd3bfc87ff56b23913c2dcdcadf06
jstella96 <jstella0530@naver.com>
2022-06-19 11:49:29 +0900

    [#15] feat: 오른쪽 사이드 리스트/마크다운 변환 버튼 및 기능 구현

---
+++ client/src/components/md-read.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="md-read" v-html="showMd" ></div>
+    </div>
+</template>
+
+<script>
+import { marked } from 'marked';
+export default {
+  computed:{
+    showMd(){
+      return marked.parse(this.desc)
+    }
+  },
+  name: 'Mdread',
+  props: {
+    desc: String
+  },
+}
+</script>
--- client/src/page/watch.vue
+++ client/src/page/watch.vue
@@ -1,21 +1,26 @@
 <template>
 <div id="watch">
-  <el-row type="flex"  justify="center">
-    <el-col :span="3" class="logo">
+  <el-row type="flex"   class="header">
+    <el-col :span="4" class="logo">
       <router-link to="/" >
         <h2 >LOGO</h2>
       </router-link>
     </el-col>
-    <el-col :span="2" :offset="17">
-      <router-link class="rigth" to="/record" ><button class="plus-button"><i class="el-icon-video-camera-solid"></i>만들기</button></router-link>
+    <el-col :span="5" :offset="15" class="header__button">
+      <router-link class="rigth" to="/record" ><button class="plus-button"><i class="el-icon-video-camera-solid"></i></button></router-link>
     </el-col>
   </el-row>
   <el-row type="flex"  justify="center">
     <el-col :span="14" class="left-side">
         <basic-video :videoKey="videoKey"></basic-video>
+            
     </el-col>
-    <el-col :span="8">
-      <list> </list> 
+    <el-col :span="8"  class="right-side">
+      <div class="right-side__header">
+      <button @click="changeLeftSide" >{{ isList ? "NOTE" : "LIST" }}</button>
+      </div>
+      <list v-if="isList"> </list> 
+      <md-read v-else :desc="desc"></md-read>
     </el-col>
   </el-row>
 </div>
@@ -24,18 +29,22 @@
 import axios from 'axios'
 import List from '../components/list.vue'
 import BasicVideo from '../components/basic-video.vue'
+import MdRead from '../components/md-read.vue'
 export default {
   name: 'Watch',
   components: { 
     "list" : List,
     "basic-video":BasicVideo,
+    "md-read":MdRead
   },
   data(){
     return{
+      isList: false,
       isControlBottom:false,
       editor:null,
       chunks:[],
-      videoKey: '1655458839773.webm'
+      videoKey: '1655458839773.webm',
+      desc: `# 운영체제 `
     }
   },
   mounted(){
@@ -52,11 +61,14 @@ export default {
         if(id){
           const response = await axios.get(`/api/videos/${id}`);
           this.videoKey = response.data.Item.videoKey
-
+          this.desc = response.data.Item.desc
         }
       }catch(err){
         console.log(err)
       }
+    },
+    changeLeftSide(){
+      this.isList = !this.isList
     }
   },
 }
